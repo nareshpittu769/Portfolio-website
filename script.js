5c82ee822523d7b3c9de83d70cb260e1817022d2
Naresh Chandragiri <nareshpittu769@gmail.com>
2023-09-14 01:11:42 +0530

    education details added

---
+++ script.js
@@ -0,0 +1,22 @@
+// function showedu(event){
+//     let btech = document.querySelector('.btech-standard')
+//     let inter = document.querySelector('.inter-standard') 
+//     let ssc = document.querySelector('.tenth-standard') 
+
+//     console.log(btech.classList);
+//     let active = event.currentTarget.classList[1]
+//     let current = event.currentTarget.classList[0]
+//     if (btech.classList[1] == 'active'){
+//         let standard = document.querySelector(`.${current}`)
+//         // console.log(standard);
+//         standard.style.display = 'block'
+//         inter.style.display = 'none'
+//         ssc.style.display = 'none'
+//     }
+//     else if{
+
+//     }
+
+// }
+
+
